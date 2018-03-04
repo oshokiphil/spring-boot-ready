ca0672398867adb8121e7aa8730cefaa3b0c1c38
Christian Dräger <christian.draeger1@gmail.com>
2018-03-04 10:39:58 +0100

    add allure reports

--- application/src/test/java/springboot/ready/api/ApiControllerIT.java
+++ application/src/test/java/springboot/ready/api/ApiControllerIT.java
@@ -1,5 +1,7 @@
 package springboot.ready.api;
 
+import static io.qameta.allure.SeverityLevel.BLOCKER;
+import static io.qameta.allure.SeverityLevel.CRITICAL;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
 import io.restassured.RestAssured;
 import springboot.ready.Application;
 
@@ -27,6 +31,8 @@ public class ApiControllerIT {
     }
 
     @Test
+    @Description("the API should respond with default values as json if no request parameters given")
+    @Severity(CRITICAL)
     public void canCallApiWithDefaults() throws Exception {
         given().when()
                 .get("/api")
@@ -37,6 +43,8 @@ public class ApiControllerIT {
     }
 
     @Test
+    @Description("the API should return the given request parameters as json")
+    @Severity(BLOCKER)
     public void canCallApiWithParameters() throws Exception {
         given().when()
                 .get("/api?firstname=carl&lastname=smith")
@@ -47,6 +55,7 @@ public class ApiControllerIT {
     }
 
     @Test
+    @Description("the API will fallback to default value if a parameter is missing")
     public void canCallApiWithParameterAndDefault() throws Exception {
         given().when()
                 .get("/api?firstname=carl")
